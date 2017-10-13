105e81f8ecc9e681ce1fd34afcf59144e918d813
chenpanjin <604970995@qq.com>
2017-10-13 18:03:08 +0800

    10.13

---
+++ app/src/main/java/com/hrht/cjp/homeview/view/IMapview.java
@@ -0,0 +1,8 @@
+package com.hrht.cjp.homeview.view;
+
+/**
+ * Created by 陈攀进 on 2017/10/13.
+ */
+
+public interface IMapview {
+}
--- app/src/main/java/com/hrht/cjp/homeview/view/MapFragment.java
+++ app/src/main/java/com/hrht/cjp/homeview/view/MapFragment.java
@@ -7,16 +7,42 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.amap.api.maps.MapView;
+
 /**
  * Created by 陈攀进 on 2017/10/12.
  */
 
 public class MapFragment extends Fragment {
+    private MapView mapView;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_map, container, false);
-
+        mapView = ((MapView) view.findViewById(R.id.fragment_left_map));
+        mapView.onCreate(savedInstanceState);
         return view;
+    }
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        mapView.onResume();
     }
 }
